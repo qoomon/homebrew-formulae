ad2e398aea9459030b9e0cff4239ce0c00883279
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-02-07 18:23:40 +0100

    Update aws-assume.rb

--- aws-assume.rb
+++ aws-assume.rb
@@ -1,12 +1,12 @@
 class AwsAssume < Formula
   desc "Easily assume AWS roles in your terminal"
   homepage "https://github.com/qoomon/aws-assume"
-  url "https://github.com/qoomon/aws-assume/releases/download/1.0.0/assume-role-Darwin"
+  url "https://github.com/qoomon/aws-assume/releases/download/v1.0.0/aws-assume-Darwin"
   version "1.0.0"
-  sha256 "68b74f3757bed9b21ea445974111c3b9ff73e7182370a76384942f4ca468b400"
+  sha256 "046ae2a7dd5d462a2a77d23775816d79e7f2cd47bd75ff8948416b9104b78ba3"
 
   def install
-    bin.install "assume-role-Darwin" => "aws-assume"
+    bin.install "aws-assume-Darwin" => "aws-assume"
   end
 
   test do
