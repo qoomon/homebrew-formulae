4f5d6b1fd4f767d715fb4fe2d68bfdbf0da1702c
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-09-03 14:06:25 +0200

    Update aws-session.rb

--- aws-session.rb
+++ aws-session.rb
@@ -2,7 +2,7 @@ class AwsSession < Formula
   desc "aws-session is a command line tool to help create temporary credentials with support for assume roles and MFA authoriztion"
   homepage "https://github.com/qoomon/aws-session"
   url "https://raw.githubusercontent.com/qoomon/aws-session/master/aws-session"
-  version "1.0.0"
+  version "1.0.1"
   sha256 "c956d0f3dd4f6268fbdd08b17dc97ba47211db2782bf86b327ff0baa75c2b890"
 
   def install
