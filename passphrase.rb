d9691fba4b81325ab2b93654e2d8bdc3db817c54
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-09-07 16:36:59 +0200

    Create passphrase.rb

---
+++ passphrase.rb
@@ -0,0 +1,13 @@
+class Passphrase < Formula
+  desc "passphrase is dice passphrase generator as described here https://www.eff.org/dice"
+  homepage "https://github.com/qoomon/passphrase"
+  url "https://github.com/qoomon/passphrase.git" :using => :git, :tag => "v1.0.0"
+  version "1.0.0"
+
+  def install
+    bin.install "passphrase"
+  end
+
+  test do
+  end
+end
