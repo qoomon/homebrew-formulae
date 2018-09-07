42e5553aeda05aeeee23c24db7a6835cf985b6c8
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-09-07 16:38:28 +0200

    passphrase.rb - syntax fix

--- passphrase.rb
+++ passphrase.rb
@@ -1,7 +1,7 @@
 class Passphrase < Formula
   desc "passphrase is dice passphrase generator as described here https://www.eff.org/dice"
   homepage "https://github.com/qoomon/passphrase"
-  url "https://github.com/qoomon/passphrase.git" :using => :git, :tag => "v1.0.0"
+  url "https://github.com/qoomon/passphrase.git", :using => :git, :tag => "v1.0.0"
   version "1.0.0"
 
   def install
