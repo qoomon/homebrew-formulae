f386026f08e2d590c17df86e0197fbd448d78e36
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-09-10 13:12:18 +0200

    Update ppgen.rb

--- ppgen.rb
+++ ppgen.rb
@@ -1,4 +1,4 @@
-class Passphrase < Formula
+class Ppgen < Formula
   desc "passphrase is dice passphrase generator as described here https://www.eff.org/dice"
   homepage "https://github.com/qoomon/passphrase-generato"
   url "https://github.com/qoomon/passphrase-generator.git", :using => :git, :tag => "v2.0.0"
