c71a929ff76c1a0309d6ab8d68dc0ffeb22098ce
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-09-10 13:03:04 +0200

    Update passphrase.rb

--- passphrase.rb
+++ passphrase.rb
@@ -1,11 +1,11 @@
 class Passphrase < Formula
   desc "passphrase is dice passphrase generator as described here https://www.eff.org/dice"
-  homepage "https://github.com/qoomon/passphrase"
-  url "https://github.com/qoomon/passphrase.git", :using => :git, :tag => "v1.0.0"
-  version "1.0.0"
+  homepage "https://github.com/qoomon/passphrase-generato"
+  url "https://github.com/qoomon/passphrase-generator.git", :using => :git, :tag => "v2.0.0"
+  version "2.0.0"
 
   def install
-    bin.install_symlink "passphrase"
+    bin.install "ppgen"
   end
 
   test do
