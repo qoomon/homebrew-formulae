30b3d28f9facc474ed039939c82af93c0e7a82b7
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-05-21 08:22:41 +0200

    Update aws-profile.rb

--- aws-profile.rb
+++ aws-profile.rb
@@ -1,9 +1,9 @@
 class AwsProfile < Formula
   desc "aws-profile is a command line tool to help creating aws profile configs"
   homepage "https://github.com/qoomon/aws-profile"
-  url "https://raw.githubusercontent.com/qoomon/aws-profile/v3.0.0/aws-profile"
-  version "3.0.0"
-  sha256 "1c1e61a0189c4cbc375f59199d841e303da3bfe1944cdc0a9394c130a209d1dd"
+  url "https://raw.githubusercontent.com/qoomon/aws-profile/v3.0.1/aws-profile"
+  version "3.0.1"
+  sha256 "39147262b7609c7c60986e7d58908e524f5dcb3456dcdbc1af878314635835a4"
 
   def install
     bin.install "aws-profile"
